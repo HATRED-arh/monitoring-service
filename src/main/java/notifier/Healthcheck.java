0c98c47896ceff37b6c7d847ced4b7da7a83d729
Edward Zelis <zeledvards@gmail.com>
2022-11-21 01:48:31 +0300

    revert url

--- src/main/java/notifier/Healthcheck.java
+++ src/main/java/notifier/Healthcheck.java
@@ -50,7 +50,7 @@ public class Healthcheck extends MessageSender {
 
 
     private void healthcheck(String domain) {
-        URI uri = URI.create(String.format("https://%s/api/healthcheck", domain));
+        URI uri = URI.create(String.format("https://%s/healthcheck", domain));
         HttpRequest request = HttpRequest.newBuilder(uri).GET()
                 .timeout(Duration.ofSeconds(10))
                 .build();
